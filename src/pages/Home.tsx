8932bda375d0e5d8907fc84ca288f78c03b35be7
Filipe Wolf Boeck <filipewolfboeck@MacBookAir.local>
2023-01-17 18:25:22 -0300

    mudanca para tsx e alteracao na Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,13 +11,23 @@ import {
 import { Button } from "../components/Button";
 import { SkillCard } from "../components/SkillCard";
 
+interface SkillData {
+  id: string;
+  name: string;
+}
+
 export default function App() {
   const [newSkill, setNewSkill] = useState("");
-  const [mySkills, setMySkills] = useState([]);
+  const [mySkills, setMySkills] = useState<SkillData[]>([]);
   const [greeting, setGreeting] = useState("");
 
   function handleAddNewSkill() {
-    setMySkills((oldState) => [...oldState, newSkill]);
+    const data = {
+      id: String(new Date().getTime()),
+      name: newSkill,
+    };
+
+    setMySkills((oldState) => [...oldState, data]);
   }
 
   useEffect(() => {
@@ -51,8 +61,8 @@ export default function App() {
 
       <FlatList
         data={mySkills}
-        keyExtractor={(item) => item}
-        renderItem={({ item }) => <SkillCard skill={item} />}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <SkillCard skill={item.name} />}
       />
     </View>
   );
@@ -62,9 +72,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#121015",
-    paddingHorizontal: 20,
-    paddingVertical: 70,
     paddingHorizontal: 30,
+    paddingVertical: 70,
   },
   title: {
     color: "#fff",
